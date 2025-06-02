0015bb9c9f819407e7b6331cabc0e5778a29a22a
HarryWarriner <harry@warriner.org>
2025-06-02 17:42:24 +0100

    slight optimisation

--- web-app/main.js
+++ web-app/main.js
@@ -1,14 +1,9 @@
 const startGame_button = document.getElementById("startGame");
-// const drawCard_button = document.getElementById("drawCard");
-// const ops_drawCard_button = document.getElementById("ops-drawCard");
-
-
-
 
 const colors = ['Red', 'Green', 'Blue', 'Yellow']
 
 let currentCard = getRandomCard();
-console.log(getRandomCard());
+console.log(currentCard);
 
 let numPlayers = 4;
 let hands =[];
@@ -22,21 +17,6 @@ startGame_button.onclick = function () {
     renderAllHands();
 }
 
-
-
-// drawCard_button.onclick = function () {
-//     if (turn === 1){
-//         drawCard();
-//     }
-    
-    
-// }
-// ops_drawCard_button.onclick = function () {
-//     if (turn === 2){
-//         drawOpsCard();
-//     } 
-    
-// }
 
 function startTurn(){
     const currentDiv = document.getElementById('currentTurn');
@@ -56,14 +36,19 @@ function getRandomCard(){
     return {color, number};
 }
 function currentCardDisplay(){
-    const currentDiv = document.getElementById('currentCard');
-    currentDiv.style.backgroundColor = currentCard.color.toLowerCase();
-    currentDiv.textContent = currentCard.number;
+    const div = document.getElementById('currentCard');
+    styleCardDiv(div, currentCard);
+   
+}
+
+function styleCardDiv(div, card){
+    div.style.backgroundColor = card.color.toLowerCase();
+    div.textContent = card.number;
 }
 
 function changeCurrentCard(){
     currentCard = getRandomCard();
-    currentCardDisplay();  
+    currentCardDisplay();
 }
 
 function drawCard(playerIndex){
@@ -112,8 +97,7 @@ function renderHand(index){
     hands[index].forEach((card, cardIndex) => {
         const div = document.createElement('div');
         div.className = 'card';
-        div.style.backgroundColor = card.color.toLowerCase();
-        div.textContent = card.number;
+        styleCardDiv(div, card);
 
         div.onclick = () => {
             if ((card.color === currentCard.color || card.number === currentCard.number) && turn === index) {
@@ -128,58 +112,3 @@ function renderHand(index){
     });
 }
 
-
-
-// function updatehand(){
-//     const handDiv = document.getElementById('hand');
-//     handDiv.innerHTML = '';
-//     hand.forEach((card, index) => {
-//         const div = renderCard(card, index);
-//         handDiv.appendChild(div)
-//     })
-// }
-
-// function updateopshand(){
-//     const opshandDiv = document.getElementById('ops-hand');
-//     opshandDiv.innerHTML = '';
-//     ops_hand.forEach((card, index) => {
-//         const div = renderOpsCard(card, index);
-//         opshandDiv.appendChild(div)
-//     })
-// }
-
-// function renderCard(card, index) {
-//     const div = document.createElement('div');
-//     div.className ='card';
-//     div.style.backgroundColor = card.color.toLowerCase();
-//     div.textContent = card.number;
-
-//     div.onclick = () => {
-//         if ((card.color === currentCard.color || card.number === currentCard.number) && turn === 1 ){
-//             hand.splice(index, 1);
-//             currentCard = card;
-//             currentCardDisplay();
-//             updatehand();
-//             currentTurn()
-//         }
-//     };
-//     return div
-// }
-
-// function renderOpsCard(card, index) {
-//     const div = document.createElement('div');
-//     div.className ='card';
-//     div.style.backgroundColor = card.color.toLowerCase();
-//     div.textContent = card.number;
-//     div.onclick = () => {
-//         if ((card.color === currentCard.color || card.number === currentCard.number) && turn === 2 ) {
-//             ops_hand.splice(index, 1);
-//             currentCard = card;
-//             currentCardDisplay();
-//             updateopshand();
-//             currentTurn()
-//         }
-//     };
-
-//     return div
-// }
