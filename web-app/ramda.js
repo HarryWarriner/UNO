35330b22bfb29e817b08c0ed700705cff4116f88
HarryWarriner <harry@warriner.org>
2025-11-16 01:51:06 +0000

    ramda fix

--- web-app/ramda.js
+++ web-app/ramda.js
@@ -1,3 +1,7 @@
+// /*jslint-disable*/
+// export * from "../node_modules/ramda/es/index.js";
+// /*jslint-enable*/
+
 /*jslint-disable*/
-export * from "../node_modules/ramda/es/index.js";
+export * from "https://cdn.jsdelivr.net/npm/ramda@0.32.0/es/index.js";
 /*jslint-enable*/
